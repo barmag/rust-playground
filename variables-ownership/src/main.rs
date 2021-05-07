2d7cc0a3382b6d4c0ef1455d1de563b593b69e53
Yasser Makram <yassermakram@gmail.com>
2021-05-07 16:17:19 +0200

    shadowing variable with changing type test

--- variables-ownership/src/main.rs
+++ variables-ownership/src/main.rs
@@ -9,8 +9,19 @@ fn shadow_variable() -> i32 {
     let test = 10;
     return test;
 }
+fn shadow_with_changing_type() -> f64 {
+    let test: i32 = 42;
+
+    let test: f64 = 42.5;
+    return test;
+}
 
 #[test]
 fn shadow_variable_test(){
     assert_eq!(shadow_variable(), 10);
+}
+
+#[test]
+fn shadow_with_type_change_test(){
+    assert_eq!(shadow_with_changing_type(), 42.5);
 }
